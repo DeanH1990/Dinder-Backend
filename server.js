c1730ac14ad59a3c2265406cd5dfd182b7809844
DeanH1990 <dhasley1990@gmail.com>
2023-03-12 15:14:51 +0000

    remove unused file

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const connection = require("./database/connection");
-
-// app.listen(9090, () => {
-//   console.log("Connected to port 9090");
-// });
