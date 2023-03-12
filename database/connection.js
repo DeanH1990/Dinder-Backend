2f81ce0df83122e347ff9060adccaccbddea6705
DeanH1990 <dhasley1990@gmail.com>
2023-03-12 15:14:42 +0000

    remove commented out code

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,6 @@
 const mongoose = require("mongoose");
 const ENV = process.env.NODE_ENV || "dev";
 
-
-// const config =
-//   ENV === "production"
-//     ? {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//           rejectUnauthorized: false,
-//         },
-//       }
-//     : {};
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -28,20 +18,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 }
-
-// connectDB();
-//
-// const mongoString = process.env.DATABASE_URL;
-// console.log(mongoString)
-
-// async function connect() {
-//   try {
-//     await mongoose.connect(mongoString);
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// connect();
 
 module.exports = connectDB;
